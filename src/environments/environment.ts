eeb28d27b4f3a05ef4ee0077887272ef65287edc
Florian Mauroner <mauroner.florian@gmail.com>
2021-11-07 11:01:57 +0100

    Firebase added with features: Firestore, Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'crossplatformadvanced-semprj',
+    appId: '1:397136810060:web:aa30362fa632c85130e4e9',
+    storageBucket: 'crossplatformadvanced-semprj.appspot.com',
+    apiKey: 'AIzaSyBB5K_Kcc8_j05nHq18ykT-ekJWkRZD4YI',
+    authDomain: 'crossplatformadvanced-semprj.firebaseapp.com',
+    messagingSenderId: '397136810060',
+    measurementId: 'G-2L0L05RFXB',
+  },
   production: false
 };
 
