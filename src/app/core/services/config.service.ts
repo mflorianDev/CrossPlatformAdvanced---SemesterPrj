3c7ed54d88d4141c8c53ebc8c52918a9301ae7af
Florian Mauroner <mauroner.florian@gmail.com>
2021-12-31 12:13:00 +0100

    ConfigService implementiert mit TrackingEnabled

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -1,22 +1,48 @@
-import { Injectable } from '@angular/core';
-import { RemoteConfig, getAll, Value, getValue, getBoolean, fetchAndActivate } from '@angular/fire/remote-config';
-import { Observable } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { RemoteConfig, fetchAndActivate, getAll, Value } from '@angular/fire/remote-config';
+import { Observable, Subscription } from 'rxjs';
+import { AllParameters } from 'rxfire/remote-config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ConfigService {
-  private config;
+export class ConfigService implements OnDestroy {
+  private config: Record<string, Value>;
+  private configObservable: Observable<AllParameters>;
+  private configSubscribtion: Subscription;
 
-  constructor( private remoteConfig: RemoteConfig ) {
-    this.fetchRemoteConfig();
+  constructor(private remoteConfig: RemoteConfig) { }
+
+  ngOnDestroy(): void {
   }
 
-  private fetchRemoteConfig(): void {
-    fetchAndActivate(this.remoteConfig).then(() => {
-      //console.log(getValue(this.remoteConfig, 'myParam').asString());
-      this.config = getAll(this.remoteConfig);
-      console.log(this.config.myParam.asString());
+  // return fetched config values
+  async getConfig(): Promise<Record<string, Value>> {
+    await this.fetchRemoteConfig();
+    return this.config;
+  }
+
+  // fetch and activate remote config from backend
+  private async fetchRemoteConfig(): Promise<void> {
+    this.remoteConfig.settings.minimumFetchIntervalMillis = 3000;
+    await fetchAndActivate(this.remoteConfig);
+    this.config = getAll(this.remoteConfig);
+  }
+
+  /*
+  // Load all configuration values from RemoteConfig and subscribe
+  private async fetchRemoteConfig(): Promise<boolean> {
+    return new Promise<boolean>((resolve) => {
+      fetchAndActivate(this.remoteConfig).then(() => {
+        this.configSubscribtion = getAll(this.remoteConfig).subscribe(
+          (params) => {
+            this.config = params;
+            console.log('params: ', params);
+            resolve(true);
+          }
+        );
+      });
     });
   }
+  */
 }
--- src/app/overview-tours/overview-tours.page.ts
+++ src/app/overview-tours/overview-tours.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { TourService } from '../core/services/tour.service';
 import { Tour } from '../core/tour';
 import { Observable } from 'rxjs';
-import { ConfigService } from '../core/services/config.service';
 
 @Component({
   selector: 'app-overview-tours',
@@ -12,7 +11,7 @@ import { ConfigService } from '../core/services/config.service';
 export class OverviewToursPage implements OnInit {
   public tours: Observable<Tour[]>;
 
-  constructor(private tourService: TourService, private configService: ConfigService) { }
+  constructor(private tourService: TourService) { }
 
   ngOnInit() {
     this.getTours();
